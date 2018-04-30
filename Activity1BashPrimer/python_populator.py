1284d54c4bcf2ad52ba5656469e843d90dccaf1a
Daniel Schoonwinkel <daniel.schoonwinkel@gmail.com>
2018-04-30 15:34:21 +0200

    Still working on Activity1. Got so far as working with python python_populator.py and using that to demonstrate background tasks

--- Activity1BashPrimer/python_populator.py
+++ Activity1BashPrimer/python_populator.py
@@ -6,8 +6,9 @@ file1.close()
 write_count = 0
 while(True):
     file1 = open("some_text.txt", 'a')
+    print("Writing to std_out %d" % write_count)
     file1.write("Writing to file %d \n" % write_count)
     write_count += 1
-    time.sleep(1)
+    time.sleep(3)
     file1.close()
 
